95b6e78ccbfdad1d5661b0bf0f53a648b45a9801
Jorge Madrid <jlmadrid360@gmail.com>
2016-04-27 22:52:21 -0500

    Add css improvements and js fixes

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -1,13 +1,22 @@
-window.onload = function() {
-  var toggle = document.getElementsByClassName('toggle');
-
-  // Iterate througth each class and add the
-  // selected class to the node that was clicked
-  for(let i = 0; i < toggle.length; i ++ ){
-    toggle[i].onclick = function(){
-      if(this.children.length > 1){
-        this.children[1].classList.toggle('selected');
+// Iterate througth each class and add the
+// selected class to the node that was clicked
+window.onclick = function(event) {
+  if (event.target.matches('.toggle') || event.target.matches('a')) {
+    var elements = document.getElementsByClassName('toggle');
+    for(let i = 0; i < elements.length; i ++ ){
+      elements[i].onclick = function(){
+        if(this.children.length > 1){
+          this.children[1].classList.toggle('selected');
+          this.classList.toggle('nav-content--selected');
+        }
+      };
+    }
+  } else {
+    var showedLists = document.getElementsByClassName("collapsible-body");
+    for (let i = 0; i < showedLists.length; i++) {
+      if (showedLists[i].classList.contains('selected')) {
+        showedLists[i].classList.remove('selected');
       }
-    };
+    }
   }
 };
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -38,22 +38,22 @@
   }
 
   // Slide the nav-bar from right to left adding 20px until is completly shown
-  function slideIn() {
+  function slideIn(){
     var slidingDiv = document.getElementsByClassName('nav-content')[0];
     var stopPosition = 0;
     if (parseInt(slidingDiv.style.left, 10) < stopPosition) {
       slidingDiv.style.left = parseInt(slidingDiv.style.left, 10) + 20 + 'px';
-      setTimeout(slideIn, .050);
+      setTimeout(slideIn, 0.50);
     }
   }
 
   // Slide the nav-bar from left to right
-  function slideOut() {
+  function slideOut(){
     var slidingDiv = document.getElementsByClassName('nav-content')[0];
     var stopPosition = -380;
     if (parseInt(slidingDiv.style.left, 10) > stopPosition) {
       slidingDiv.style.left = parseInt(slidingDiv.style.left, 10) - 20 + 'px';
-      setTimeout(slideOut, .050);
+      setTimeout(slideOut, 0.50);
     }
   }
 
